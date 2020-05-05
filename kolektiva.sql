e81079027e066e96d8f800f09941a0ff3bb430d5
Vicky Anggara <vickyanggara16@gmail.com>
2020-05-05 13:57:14 +0700

    Add files via upload

--- kolektiva.sql
+++ kolektiva.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2020 at 06:18 PM
+-- Generation Time: May 05, 2020 at 08:55 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.3.13
 
